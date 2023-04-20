388909b98e791bf79cf76a9a862029d9c0266756
ilyaryhth <cool.clarion@yandex.ru>
2023-04-20 09:32:49 +0400

    Animal an3 add

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -12,6 +12,11 @@ public class Test {
                 .id(2)
                 .build();
 
+        Animal an3 = Animal.builder()
+                .name("Cat2")
+                .id(2)
+                .build();
+
         System.out.println(an1);
 
         System.out.println(OffsetDateTime.now().minusDays(50));
